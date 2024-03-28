9ff2a3d4904f3f23b273ea71f2c1dfbf836160c0
medkhalilbensmida <159784532+medkhalilbensmida@users.noreply.github.com>
2024-03-28 02:00:16 +0100

    Delete distributed-system-main/distributed-system-main directory

--- distributed-system-main/distributed-system-main/RMI/TaskList.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.List;
-
-public interface TaskList extends Remote {
-    void addTask(String task) throws RemoteException;
-    void removeTask(String task) throws RemoteException;
-    List<String> getTasks() throws RemoteException;
-}
